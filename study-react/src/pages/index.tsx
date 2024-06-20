ff87be282b4f15c00e1a43a327a985d9367bcc27
rita-228 <hikaru228.work@gmail.com>
2024-06-20 17:05:00 +0900

    Update index.tsx

--- study-react/src/pages/index.tsx
+++ study-react/src/pages/index.tsx
@@ -32,7 +32,7 @@ export default function Home() {
             By{" "}
             <Image
               src="/vercel.svg"
-              alt="Vercel Logo"
+              alt="Vcel Logo"
               className="dark:invert"
               width={100}
               height={24}
